ccd18d062b06799f95037badd597aeacf5712b5c
an-Iceberg <a.g.marosi@bluewin.ch>
2023-05-05 12:11:29 +0200

    Added styling and positioning to example window

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use macroquad::prelude::*;
+use egui::{*, epaint::Shadow};
 
 fn window_configuration() -> Conf
 {
@@ -22,10 +23,29 @@ async fn main()
 
     // Process keys, mouse etc.
 
-    egui_macroquad::ui(|egui_ctx| {
+    egui_macroquad::ui(|egui_ctx|
+    {
+      // Disabling all shadows on all windows
+      egui_ctx.set_visuals(egui::Visuals{ window_shadow: Shadow::NONE, window_rounding: Rounding { nw: 10., ne: 0., sw: 10., se: 0. }, ..Default::default() });
+
       egui::Window::new("egui ❤ macroquad")
-        .show(egui_ctx, |ui| {
-            ui.label("Test");
+        .anchor(Align2::RIGHT_TOP, egui::Vec2::new(0., 0.))
+        .constrain(true)
+        .collapsible(false)
+        .movable(false)
+        .resizable(false)
+        // ? Why is the height not working?
+        .default_height(700.)
+        .min_height(700.)
+        .fixed_size(egui::vec2(200., 700.))
+        // .frame(Frame { inner_margin: (), outer_margin: (), rounding: (), shadow: (), fill: (), stroke: () }) // Useful
+        .show(egui_ctx, |ui|
+        {
+          // ui.style_mut().visuals.window_shadow = Shadow::NONE;
+          // ui.style_mut().visuals.window_rounding = Rounding { nw: 10., ne: 0., sw: 10., se: 0. };
+          ui.label("Hello World");
+          ui.separator();
+          ui.label("Hello again");
         });
     });
 
