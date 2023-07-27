5d5f2c1ddbcd6ec6f8743656c7fffa9174c8726c
an-Iceberg <a.g.marosi@bluewin.ch>
2023-07-27 18:48:15 +0200

    Restructuring; minor additions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use macroquad::prelude::*;
-use egui::{*, epaint::Shadow};
 
 fn window_configuration() -> Conf
-{
-  return Conf
-  {
-    window_title: "egui ❤ macroquad".to_string(),
+{ return Conf
+  { window_title: "egui ❤ macroquad".to_string(),
     window_width: 1290,
     window_height: 720,
     fullscreen: false,
@@ -16,36 +13,58 @@ fn window_configuration() -> Conf
 
 #[macroquad::main(window_configuration)]
 async fn main()
-{
+{ let mut mode = Mode::Move;
+
   loop
-  {
-    clear_background(WHITE);
+  { clear_background(WHITE);
 
     // Process keys, mouse etc.
 
     egui_macroquad::ui(|egui_ctx|
-    {
-      // Disabling all shadows on all windows
-      egui_ctx.set_visuals(egui::Visuals{ window_shadow: Shadow::NONE, window_rounding: Rounding { nw: 10., ne: 0., sw: 10., se: 0. }, ..Default::default() });
+    // Disabling all shadows on all windows
+    { egui_ctx.set_visuals(egui_macroquad::egui::Visuals
+      { window_shadow: egui_macroquad::egui::epaint::Shadow::NONE,
+        window_rounding: egui_macroquad::egui::Rounding
+        { nw: 10.,
+          ne: 0.,
+          sw: 10.,
+          se: 0.,
+        },
+        ..Default::default()
+      });
 
       egui::Window::new("egui ❤ macroquad")
-        .anchor(Align2::RIGHT_TOP, egui::Vec2::new(0., 0.))
+        .anchor(egui::Align2::RIGHT_TOP, egui::Vec2::new(0., 10.))
         .constrain(true)
         .collapsible(false)
         .movable(false)
         .resizable(false)
         // ? Why is the height not working?
-        .default_height(700.)
-        .min_height(700.)
+        //.default_height(f32::INFINITY)
+        //.min_height(700.)
         .fixed_size(egui::vec2(200., 700.))
         // .frame(Frame { inner_margin: (), outer_margin: (), rounding: (), shadow: (), fill: (), stroke: () }) // Useful
         .show(egui_ctx, |ui|
-        {
-          // ui.style_mut().visuals.window_shadow = Shadow::NONE;
+        { // ui.style_mut().visuals.window_shadow = Shadow::NONE;
           // ui.style_mut().visuals.window_rounding = Rounding { nw: 10., ne: 0., sw: 10., se: 0. };
-          ui.label("Hello World");
+
+          ui.label("Mode");
+          ui.horizontal(|ui|
+          { ui.radio_value(&mut mode, Mode::Move, "Move");
+            ui.radio_value(&mut mode, Mode::Line, "Line");
+            ui.radio_value(&mut mode, Mode::Point, "Point");
+            ui.radio_value(&mut mode, Mode::Path, "Path");
+          });
+          match mode
+          { Mode::Move => ui.label("Left click on a point to select it and hold left click to move it around."),
+            Mode::Line => ui.label("Left click on a point to select it and left click on another point to create a line or right click to delete an existing line."),
+            Mode::Point => ui.label("Left click somewhere to create a point or right click on a point to delete it."),
+            Mode::Path => ui.label("Left click on a point to set the start and right click on a point to set the end.")
+          };
+
           ui.separator();
           ui.label("Hello again");
+          ui.add_space(500.);
         });
     });
 
@@ -57,4 +76,9 @@ async fn main()
 
     next_frame().await;
   }
+}
+
+#[derive(PartialEq, Eq)]
+enum Mode
+{ Move, Point, Line, Path
 }
