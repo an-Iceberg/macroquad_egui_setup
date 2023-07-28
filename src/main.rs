5d3ff28d197043ac6b4b668107844111d233c559
an-Iceberg <a.g.marosi@bluewin.ch>
2023-07-28 09:52:27 +0200

    Added "tab" selector; "fixed" differing text heights; resolved compilation issues with egui and macroquad

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,16 @@ async fn main()
         { // ui.style_mut().visuals.window_shadow = Shadow::NONE;
           // ui.style_mut().visuals.window_rounding = Rounding { nw: 10., ne: 0., sw: 10., se: 0. };
 
+          ui.label("Select a mode");
+          ui.horizontal(|ui|
+          { ui.selectable_value(&mut mode, Mode::Move, "Move");
+            ui.selectable_value(&mut mode, Mode::Line, "Line");
+            ui.selectable_value(&mut mode, Mode::Point, "Point");
+            ui.selectable_value(&mut mode, Mode::Path, "Path");
+          });
+
+          ui.separator();
+
           ui.label("Mode");
           ui.horizontal(|ui|
           { ui.radio_value(&mut mode, Mode::Move, "Move");
@@ -55,16 +65,18 @@ async fn main()
             ui.radio_value(&mut mode, Mode::Point, "Point");
             ui.radio_value(&mut mode, Mode::Path, "Path");
           });
+
+          // The newlines are a hack to make all text fill up the same amount of vertical space
           match mode
-          { Mode::Move => ui.label("Left click on a point to select it and hold left click to move it around."),
+          { Mode::Move => ui.label("Left click on a point to select it and hold left click to move it around.\n\n"),
             Mode::Line => ui.label("Left click on a point to select it and left click on another point to create a line or right click to delete an existing line."),
-            Mode::Point => ui.label("Left click somewhere to create a point or right click on a point to delete it."),
-            Mode::Path => ui.label("Left click on a point to set the start and right click on a point to set the end.")
+            Mode::Point => ui.label("Left click somewhere to create a point or right click on a point to delete it.\n"),
+            Mode::Path => ui.label("Left click on a point to set the start and right click on a point to set the end.\n")
           };
 
           ui.separator();
           ui.label("Hello again");
-          ui.add_space(500.);
+          ui.add_space(400.);
         });
     });
 
